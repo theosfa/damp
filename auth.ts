2c2075944d49e9c248a62d0b13f941f8e1758492
theosfa <theos.fa@gmail.com>
2024-10-03 15:30:05 +0100

    if table doens't exist there is a problem to add user

--- auth.ts
+++ auth.ts
@@ -48,6 +48,17 @@ export async function signUp(
     return { error: 'User already exists' };
   }
 
+  await client.sql`
+    CREATE TABLE IF NOT EXISTS users (
+      id UUID DEFAULT uuid_generate_v4() PRIMARY KEY,
+      name VARCHAR(255) NOT NULL,
+      email TEXT NOT NULL UNIQUE,
+      password TEXT NOT NULL,
+      task_ids JSON,
+      project_ids JSON
+    );
+  `;
+  
   // Insert new user into the database
   const user = await client.sql`
     INSERT INTO users (name, email, password, task_ids, project_ids)
